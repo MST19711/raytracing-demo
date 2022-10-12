28e3b59d19857648bc702f21f46410ff765c5ccb
Chengxi Li <chengxili@smail.nju.edu.cn>
2022-10-12 21:00:24 +0800

    4

--- Ray_tracing_DEMO/main.cpp
+++ Ray_tracing_DEMO/main.cpp
@@ -2,18 +2,39 @@
 #include<fstream>
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
 
-#define IMAGE_WIDTH 256
-#define IMAGE_HEIGHT 256
+
+color rayColor(const ray& r){
+    vec3 unitDirection = unitVector(r.direction());
+    auto t = 0.5*(unitDirection.y()+1);
+    return (1.0 - t)*color(1.0,1.0,1.0) + t*color(0.5,0.7,1.0);
+}
 
 int main(){
+    //image
+    const auto aspectRatio = 16.0/9.0;
+    const int imageWidth = 400;
+    const int imageHeight = static_cast<int>(imageWidth/aspectRatio);
+    //camera
+    auto viewpointHeight = 2.0;
+    auto viewpointWidth = viewpointHeight * aspectRatio;
+    auto facilLength = 1.0;
+    auto orignPoint = point3(0.0,0.0,0.0);
+    auto horizontal = vec3(viewpointWidth,0.0,0.0);
+    auto vertical = vec3(0.0,viewpointHeight,0.0);
+    auto lowerLeftCorner = orignPoint - horizontal/2 - vertical/2 -vec3(0.0,0.0,facilLength);
+    //render
     std::ofstream outPutImg;
     outPutImg.open("image.ppm");
     std::cout<<"Writting image...\n";
-    outPutImg<<"P3\n"<<IMAGE_WIDTH<<" "<<IMAGE_HEIGHT<<"\n255\n";
-    for(int j = IMAGE_HEIGHT-1;j >= 0;j--){
-        for(int i = 0;i < IMAGE_WIDTH;i++){
-            color pixelColor(double(i)/double(IMAGE_WIDTH-1),double(j)/double(IMAGE_HEIGHT-1),0.25);
+    outPutImg<<"P3\n"<<imageWidth<<" "<<imageHeight<<"\n255\n";
+    for(int j = imageHeight-1;j >= 0;j--){
+        for(int i = 0;i < imageWidth;i++){
+            auto u = double(i)/(imageWidth - 1);
+            auto v = double(j)/(imageHeight - 1);
+            ray r(orignPoint,lowerLeftCorner + u*horizontal + v*vertical);
+            color pixelColor = rayColor(r);
             writeColor(outPutImg,pixelColor);
         }
     }
